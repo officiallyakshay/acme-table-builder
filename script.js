c2f368d0231953ec8c35d220b60c2037066da1ca
casualshay <akshaysprabhakar@gmail.com>
2019-08-01 15:55:06 -0700

    complete

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ class TableBuilder {
 
   init() {
     this.render();
+
+    document.querySelector(this.selector).addEventListener('click', (ev) => {
+      if (ev.target.tagName === 'TH') {
+        const idx = [...ev.target.parentNode.children].indexOf(ev.target);
+        const header = this.columnHeader[idx];
+        const key = header.key;
+        this.data = this.data.sort((a, b) => {
+          if (header.type === 'number') {
+            return a[key] - b[key];
+          }
+          return a[key].localeCompare(b[key]);
+        });
+        this.render();
+      }
+    });
+
   }
 
   render() {
-    const tableHeaders = `<tr>${this.columnHeader.map(header => `<th>${header.title}</th>`)}</tr>
-    <tr>${this.data.map(obj => `<td>${Object.values(obj)}</td>`)}</tr>`
+    const tableHeaders = `<tr>${this.columnHeader.map(header => `<th>${header.key}</th>`).join('')}</tr>
+    ${this.data.map(item => `<tr>${this.columnHeader.map(header => `<td>${item[header.key]}</td>`).join('')}</tr>`)}`
     document.querySelector(this.selector).innerHTML = tableHeaders;
   }
 
